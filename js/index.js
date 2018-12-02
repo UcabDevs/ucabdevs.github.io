830839a2d264b7da8a4a4dfed8b24fd5dd85cb3f
alessandro <aless0117@outlook.es>
2018-12-01 20:59:40 -0430

    New section and modal. Fixed some css classes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
-//COMANDO PARA DESPLEGAR MODAL
-// $('#pregModal').modal("show")
-
 
 let submitted = false;
 
 function whatIsHacker() {
   alert('es todo individuo que se dedica a programar de forma entusiasta, o sea un experto entusiasta de cualquier tipo')
 }
-
